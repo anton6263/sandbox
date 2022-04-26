aa07919a1520e95ed563a3f597a7d0ddc74fe4c6
anton6263 <anton.arteeff@yandex.ru>
2022-04-26 18:16:05 +0300

    Создание проекта в IDEA

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,9 @@
+import com.sun.source.util.SourcePositions;
+
+public class MyFirstProgram {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
