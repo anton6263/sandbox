cb22eb387d10f41352c42775a258baed83e53af2
anton6263 <anton.arteeff@yandex.ru>
2022-04-30 12:57:35 +0300

    Задание №2

--- src/main/java/Distance.java
+++ src/main/java/Distance.java
@@ -1,7 +1,8 @@
 public class Distance {
     public static void main(String[] args) {
-        Point p1 = new Point(5, 5);
-        Point p2 = new Point(4, 5);
+        Point p1 = new Point(5, 0);
+        Point p2 = new Point(4, 2);
+
 
         System.out.println("Расстояние между двумя точками p1 и p2 равно " + p1.distance(p2));
     }
--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,15 +1,15 @@
 public class Point {
-    public double x1, y1;
-    public double x2, y2;
-    public Point p1(double x1, double y1) {
-        this.x1 = x1;
-        this.y1 = y1;
+
+    public double x;
+    public double y;
+
+    public Point(double p1, double p2) {
+        this.x = p1;
+        this.y = p2;
     }
-    public Point p2(double x2, double y2) {
-        this.x2 = x2;
-        this.y2 = y2;
+
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
-    public double distance() {
-        return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
-    }
+
 }
